eb4d8bc0a81a91534f08d7ccee3997f2acd1ef7b
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-11-19 09:58:27 -0400

    fiz o fetch de pegar os dados do product clicado da oferta relampago

--- src/Components/FlashSaleProductComponents/FlashSaleProduct/FlashSaleProduct.tsx
+++ src/Components/FlashSaleProductComponents/FlashSaleProduct/FlashSaleProduct.tsx
@@ -1,20 +1,85 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import HeaderFlashSaleMain from '../HeaderFlashSaleComponents/HeaderFlashSaleMain/HeaderFlashSaleMain';
+import { Url } from '../../../Utils/Url';
+import * as Styled from './styled';
+
+interface GetFlashSaleProductProps {
+  id: string;
+  productReviewsRate: number;
+  productsOfferFlashDTO: ProductsOfferFlashDTOProps;
+  quantitySold: number;
+}
+
+interface ProductsOfferFlashDTOProps {
+  altValue: string;
+  discountPercentage: number;
+  imgPartBottom: string;
+  imgProduct: string;
+  priceProduct: number;
+  title: string;
+}
 
 const FlashSaleProduct = () => {
   const obj = useParams();
+  const nav = useNavigate();
+  const [getFlashSaleProduct, setGetFlashSaleProduct] = useState<GetFlashSaleProductProps | null>(
+    null
+  );
 
   useEffect(() => {
     const { id } = obj;
-    console.log(id);
+
+    if (id) {
+      getFlashSaleProductByProductFlashSaleId(id);
+    }
   }, []);
 
+  const getFlashSaleProductByProductFlashSaleId = async (productFlashSaleId: string) => {
+    const res = await fetch(
+      `${Url}/get-flash-sale-product-by-product-flash-sale-id/${productFlashSaleId}`
+    );
+
+    if (res.status === 200) {
+      const json = await res.json();
+      let getFlashSaleProduct: GetFlashSaleProductProps = json.data;
+      setGetFlashSaleProduct(getFlashSaleProduct);
+    }
+
+    if (res.status === 400) {
+      //ERROR
+    }
+
+    if (res.status === 403 || res.status === 401) {
+      localStorage.removeItem('user');
+      nav('/login');
+      return;
+    }
+  };
+
   return (
-    <div>
+    <Styled.ContainerMain>
       <HeaderFlashSaleMain></HeaderFlashSaleMain>
-      <h1>Product Clicked Seila</h1>
-    </div>
+      <Styled.ContainerProductWasClickedMain>
+        <Styled.ContainerProductWasClicked>
+          <Styled.ContainerProductLayers>
+            <Styled.Span>Shopee</Styled.Span>
+            <Styled.Span>{'>'}</Styled.Span>
+            <Styled.Span>Saúde</Styled.Span>
+            <Styled.Span>{'>'}</Styled.Span>
+            <Styled.Span>Cuidados Pessoais</Styled.Span>
+            <Styled.Span>{'>'}</Styled.Span>
+            <Styled.Span>Cuidados Bucais</Styled.Span>
+            <Styled.Span>{'>'}</Styled.Span>
+            {getFlashSaleProduct && (
+              <Styled.Span>
+                {getFlashSaleProduct.productsOfferFlashDTO.title.substring(0, 50)}
+              </Styled.Span>
+            )}
+          </Styled.ContainerProductLayers>
+        </Styled.ContainerProductWasClicked>
+      </Styled.ContainerProductWasClickedMain>
+    </Styled.ContainerMain>
   );
 };
 
---
+++ src/Components/FlashSaleProductComponents/FlashSaleProduct/styled.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+`;
+
+export const Img = styled.img`
+`;
+
+export const Span = styled.span`
+`;
+
+export const Input = styled.input`
+`;
+
+export const Button = styled.button`
+`;
+
+export const Container = styled.div`
+`;
+
+export const ContainerMain = styled.div`
+`;
+
+export const ContainerProductWasClickedMain = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 21px;
+  margin-top: 6px;
+`;
+
+export const ContainerProductWasClicked = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 1200px;
+`;
+
+export const ContainerProductLayers = styled.div`
+  display: flex;
+`;
