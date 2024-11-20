bd5818b99e215c924b42ec6921baa1c54d9032d4
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-11-20 09:56:55 -0400

    mechendo no component quando cliquei no product flash offer

--- src/Components/FlashSaleProductComponents/FlashSaleProduct/FlashSaleProduct.tsx
+++ src/Components/FlashSaleProductComponents/FlashSaleProduct/FlashSaleProduct.tsx
@@ -3,12 +3,15 @@ import { useNavigate, useParams } from 'react-router-dom';
 import HeaderFlashSaleMain from '../HeaderFlashSaleComponents/HeaderFlashSaleMain/HeaderFlashSaleMain';
 import { Url } from '../../../Utils/Url';
 import * as Styled from './styled';
+import SvgArrowBottom from '../../Svg/SvgArrowBottom/SvgArrowBottom';
+import HeartRed from '../../Svg/HeartRed/HeartRed';
 
 interface GetFlashSaleProductProps {
   id: string;
   productReviewsRate: number;
   productsOfferFlashDTO: ProductsOfferFlashDTOProps;
   quantitySold: number;
+  favoriteQuantity: number;
 }
 
 interface ProductsOfferFlashDTOProps {
@@ -43,6 +46,7 @@ const FlashSaleProduct = () => {
     if (res.status === 200) {
       const json = await res.json();
       let getFlashSaleProduct: GetFlashSaleProductProps = json.data;
+
       setGetFlashSaleProduct(getFlashSaleProduct);
     }
 
@@ -57,27 +61,106 @@ const FlashSaleProduct = () => {
     }
   };
 
+  const funcFormatFavoriteQuantity = (favoriteQuantity: number) => {
+    if (favoriteQuantity >= 1000) {
+      // Divide por 1000 e formata com uma casa decimal
+
+      const formattedQuantity = (favoriteQuantity / 1000).toFixed(1).replace('.', ',');
+      return `Favoritar (${formattedQuantity}mil)`;
+    } else {
+      // Retorna o número sem formatação especial
+      return `Favoritar (${favoriteQuantity})`;
+    }
+  };
+
+  const funcFormatTitleProduct = (productsOfferFlashTitle: string) => {
+    let stringTitle = productsOfferFlashTitle.slice(0, 120) + '...';
+
+    return stringTitle;
+  };
+
   return (
     <Styled.ContainerMain>
       <HeaderFlashSaleMain></HeaderFlashSaleMain>
       <Styled.ContainerProductWasClickedMain>
-        <Styled.ContainerProductWasClicked>
-          <Styled.ContainerProductLayers>
-            <Styled.Span>Shopee</Styled.Span>
-            <Styled.Span>{'>'}</Styled.Span>
-            <Styled.Span>Saúde</Styled.Span>
-            <Styled.Span>{'>'}</Styled.Span>
-            <Styled.Span>Cuidados Pessoais</Styled.Span>
-            <Styled.Span>{'>'}</Styled.Span>
-            <Styled.Span>Cuidados Bucais</Styled.Span>
-            <Styled.Span>{'>'}</Styled.Span>
-            {getFlashSaleProduct && (
-              <Styled.Span>
+        {getFlashSaleProduct && (
+          <Styled.ContainerProductWasClicked>
+            <Styled.ContainerProductLayers>
+              <Styled.Span>Shopee</Styled.Span>
+              <SvgArrowBottom />
+              <Styled.Span>Saúde</Styled.Span>
+              <SvgArrowBottom />
+              <Styled.Span>Cuidados Pessoais</Styled.Span>
+              <SvgArrowBottom />
+              <Styled.Span>Cuidados Bucais</Styled.Span>
+              <SvgArrowBottom />
+              <Styled.H1>
                 {getFlashSaleProduct.productsOfferFlashDTO.title.substring(0, 50)}
-              </Styled.Span>
-            )}
-          </Styled.ContainerProductLayers>
-        </Styled.ContainerProductWasClicked>
+              </Styled.H1>
+            </Styled.ContainerProductLayers>
+
+            <Styled.ContainerImageProductAndDescription>
+              <Styled.ContainerImageProductAndAllImagePartBottom>
+                <Styled.ContainerImageProduct>
+                  <Styled.Img
+                    src={getFlashSaleProduct.productsOfferFlashDTO.imgProduct}
+                    alt={getFlashSaleProduct.productsOfferFlashDTO.altValue}
+                  />
+
+                  <Styled.Img
+                    src={getFlashSaleProduct.productsOfferFlashDTO.imgPartBottom}
+                    alt="img-part-bottom-floating"
+                  />
+                </Styled.ContainerImageProduct>
+
+                <Styled.ContainerAllImagePartBottom>
+                  {/* criar uma tabela para colocar as imagens de baixo! - que pega o id do "product" e nessa tabela
+                      vai ter o id ai eu pucho todas as "imagens" dessa tabela com a imagens */}
+                  <Styled.Img
+                    src={getFlashSaleProduct.productsOfferFlashDTO.imgProduct}
+                    alt={getFlashSaleProduct.productsOfferFlashDTO.altValue}
+                  />
+                  <Styled.Img
+                    src={getFlashSaleProduct.productsOfferFlashDTO.imgProduct}
+                    alt={getFlashSaleProduct.productsOfferFlashDTO.altValue}
+                  />
+                  <Styled.Img
+                    src={getFlashSaleProduct.productsOfferFlashDTO.imgProduct}
+                    alt={getFlashSaleProduct.productsOfferFlashDTO.altValue}
+                  />
+                  <Styled.Img
+                    src={getFlashSaleProduct.productsOfferFlashDTO.imgProduct}
+                    alt={getFlashSaleProduct.productsOfferFlashDTO.altValue}
+                  />
+                </Styled.ContainerAllImagePartBottom>
+
+                <Styled.ContainerToShareMain>
+                  <Styled.ContainerToShare>
+                    <Styled.H1>Compartilhar:</Styled.H1>
+                    <Styled.Button></Styled.Button>
+                    <Styled.Button></Styled.Button>
+                    <Styled.Button></Styled.Button>
+                    <Styled.Button></Styled.Button>
+                  </Styled.ContainerToShare>
+
+                  <Styled.ContainerFavoriteQuantity>
+                    <HeartRed />
+
+                    <Styled.Span>
+                      {funcFormatFavoriteQuantity(getFlashSaleProduct.favoriteQuantity)}
+                    </Styled.Span>
+                  </Styled.ContainerFavoriteQuantity>
+                </Styled.ContainerToShareMain>
+              </Styled.ContainerImageProductAndAllImagePartBottom>
+
+              <Styled.ContainerProductFlashSaleDescription>
+                <Styled.H1>
+                  {funcFormatTitleProduct(getFlashSaleProduct.productsOfferFlashDTO.title)}
+                </Styled.H1>
+              </Styled.ContainerProductFlashSaleDescription>
+            </Styled.ContainerImageProductAndDescription>
+          </Styled.ContainerProductWasClicked>
+        )}
       </Styled.ContainerProductWasClickedMain>
     </Styled.ContainerMain>
   );
--- src/Components/FlashSaleProductComponents/FlashSaleProduct/styled.ts
+++ src/Components/FlashSaleProductComponents/FlashSaleProduct/styled.ts
@@ -37,4 +37,140 @@ export const ContainerProductWasClicked = styled.div`
 
 export const ContainerProductLayers = styled.div`
   display: flex;
+  align-items: center;
+  gap: 6px;
+  margin-bottom: 40px;
+
+  >span {
+    font-size: 14px;
+    color: rgb(0, 85, 170);
+  }
+
+  >svg {
+    width: 12px;
+    height: 12px;
+    fill: #000000a6;
+
+    transform: rotate(270deg);
+  }
+
+  >h1 {
+    font-size: 15px;
+    font-weight: 400;
+  }
+`;
+
+export const ContainerImageProductAndDescription = styled.div`
+  display: flex;
+`;
+
+export const ContainerImageProductAndAllImagePartBottom = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 480px;
+`;
+
+export const ContainerAllImagePartBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 460px;
+  margin-bottom: 30px;
+
+  >img {
+    width: 100px;
+    height: auto;
+    object-fit: cover;
+  }
+`;
+
+export const ContainerToShareMain = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const ContainerToShare = styled.div`
+  display: flex;
+  align-items: center;
+
+  >h1 {
+    font-size: 18px;
+    font-weight: 400;
+  }
+
+  > button {
+    border: 0;
+    height: 25px;
+    margin-left: 5px;
+    outline: 0;
+    overflow: visible;
+    padding: 0;
+    position: relative;
+    width: 25px;
+
+    /* background-position: 0 0; */
+    background-color: initial;
+    background-image: url(https://res.cloudinary.com/dyqsqg7pk/image/upload/v1732105018/12f585f9c56d4f30_r9l0vi.png);
+    background-size: 100%;
+  }
+
+  > button:nth-of-type(1) {
+    background-position: 0 -100%;
+  }
+
+  > button:nth-of-type(2){
+    background-position: 0  0;
+  }
+
+  > button:nth-of-type(3){
+    background-position: 0 -300%;
+  }
+
+  > button:nth-of-type(4){
+    background-position: 0 -400%;
+  }
+`;
+
+export const ContainerFavoriteQuantity = styled.div`
+  display: flex;
+  align-items: center;
+  line-height: 1.1;
+
+  >span {
+    font-size: 18px;
+    width: 100px;
+    text-align: center;
+  }
+`;
+
+export const ContainerProductFlashSaleDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 720px;
+
+  >h1 {
+    font-size: 20px;
+    font-weight: 500;
+    color: rgba(0, 0, 0, 0.8);
+  }
+`;
+
+export const ContainerImageProduct = styled.div`
+  display: flex;
+  width: 450px;
+  position: relative;
+
+  > img:nth-of-type(1){
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+
+  > img:nth-of-type(2) {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
 `;
